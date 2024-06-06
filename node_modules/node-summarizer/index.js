d2200bbd80fb8e5ee5a4980169b6907b6e6821f3
r4ghuveer <raghuveerofficial08@gmail.com>
2024-06-06 20:51:10 +0530

    -

--- node_modules/node-summarizer/index.js
+++
@@ -1,1 +0,0 @@
-module.exports.SummarizerManager = require("./src/SummarizerManager");
--- routes/home.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const homeController = require("../controllers/home")
-router.get('/',homeController.gethome);
-router.post('/',homeController.postSummary);
-
-module.exports = router;
